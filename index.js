126e4d45562fc091385045acb857fa8e8deb1448
ndtan <ndtanqb3k@gmail.com>
2018-11-08 11:11:15 +0700

    Update index.js
    Fix lỗi biên dịch trên các phiên bản react native mới

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import {
   View,
   WebView,
   StyleSheet,
+  ViewPropTypes
 } from 'react-native';
+
+import PropTypes from 'prop-types';
 
 
 import htmlContent from './injectedHtml';
@@ -19,7 +22,7 @@ class SignaturePad extends Component {
   static propTypes = {
     onChange: PropTypes.func,
     onError: PropTypes.func,
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
     penColor: PropTypes.string,
     dataURL: PropTypes.string,
   };
@@ -123,7 +126,8 @@ class SignaturePad extends Component {
                  renderLoading={this._renderLoading}
                  source={this.source}
                  javaScriptEnabled={true}
-                 style={this.props.style}/>
+                 style={this.props.style}
+                 originWhitelist={['*']}/>
     )
   };
 }
